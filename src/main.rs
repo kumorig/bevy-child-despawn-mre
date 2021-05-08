352c88df0f119c139871cd1fa89cdcd7c9f1be4f
Marcus Sakae <sakae.marcus@gmail.com>
2021-05-08 18:14:36 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use bevy::{app::AppExit, core::FixedTimestep, prelude::*};
+
+fn main() {
+    App::build()
+        .add_plugins(MinimalPlugins)
+        .add_startup_system(setup.system())
+        .add_system(count_children.system())
+        .add_system(despawn_children.system())
+        .add_system_set(SystemSet::new().with_system(stop.system()).with_run_criteria(FixedTimestep::step(0.01)))
+        .run();
+}
+
+struct SomeParent {}
+struct SomeChild {}
+
+fn stop(mut exit_events: EventWriter<AppExit>){
+   exit_events.send(AppExit);
+}
+
+fn despawn_children(mut commands: Commands, child_query: Query<(&SomeChild, Entity)>) {
+    // First time 3, after that 0 .
+   println!("child_query.iter().len(): {}", child_query.iter().len());
+   // Despawns all children
+   child_query
+        .iter()
+        .for_each(|(_, e)| commands.entity(e).despawn());     
+}
+
+fn count_children(q: Query<&Children, With<SomeParent>>) {
+   // First time 3, after that... 3 ???
+   q.iter().for_each(|children| {
+       println!("children.len(): {}", children.len());
+   });
+}
+
+
+fn setup(mut commands: Commands) {
+    commands.spawn_bundle((SomeParent {},)).with_children(|p| {
+        p.spawn_bundle((SomeChild {},));
+        p.spawn_bundle((SomeChild {},));
+        p.spawn_bundle((SomeChild {},));
+    });
+}
