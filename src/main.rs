d71cd8cc4032ff3cacd6f650eac976fd3a33e063
Marcus Sakae <sakae.marcus@gmail.com>
2021-05-08 18:20:56 +0900

    ...

--- src/main.rs
+++ src/main.rs
@@ -6,33 +6,36 @@ fn main() {
         .add_startup_system(setup.system())
         .add_system(count_children.system())
         .add_system(despawn_children.system())
-        .add_system_set(SystemSet::new().with_system(stop.system()).with_run_criteria(FixedTimestep::step(0.01)))
+        .add_system_set(
+            SystemSet::new()
+                .with_system(stop.system())
+                .with_run_criteria(FixedTimestep::step(0.01)),
+        )
         .run();
 }
 
 struct SomeParent {}
 struct SomeChild {}
 
-fn stop(mut exit_events: EventWriter<AppExit>){
-   exit_events.send(AppExit);
+fn stop(mut exit_events: EventWriter<AppExit>) {
+    exit_events.send(AppExit);
 }
 
 fn despawn_children(mut commands: Commands, child_query: Query<(&SomeChild, Entity)>) {
     // First time 3, after that 0. (All good)
-   println!("child_query.iter().len(): {}", child_query.iter().len());
-   // Despawns all children
-   child_query
+    println!("child_query.iter().len(): {}", child_query.iter().len());
+    // Despawns all children
+    child_query
         .iter()
-        .for_each(|(_, e)| commands.entity(e).despawn());     
+        .for_each(|(_, e)| commands.entity(e).despawn());
 }
 
 fn count_children(q: Query<&Children, With<SomeParent>>) {
-   // First time 3, after that... 3 ???
-   q.iter().for_each(|children| {
-       println!("children.len(): {}", children.len());
-   });
+    // First time 3, after that... 3 ???
+    q.iter().for_each(|children| {
+        println!("children.len(): {}", children.len());
+    });
 }
-
 
 fn setup(mut commands: Commands) {
     commands.spawn_bundle((SomeParent {},)).with_children(|p| {
